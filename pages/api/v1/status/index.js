8f394341b2522c2cf4776ae7d03686bf17b2c3f8
Alexandre Torturela <154904640+AlexandreTorturela@users.noreply.github.com>
2025-12-27 15:48:08 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  response.status(200).json({ chave: "Alunos do curso.dev são excelentes!" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersion = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConnections = dbMaxConnectionsResult.rows[0].max_connections;
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [dbName],
+  });
+  const dbOpenConnections = dbOpenConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersion,
+        max_connections: parseInt(dbMaxConnections),
+        opened_connections: dbOpenConnections,
+      },
+    },
+  });
 }
 
 export default status;
