4c790d67241d677e33f03dd026d1cebd4b0f5cd7
Alexandre Torturela <154904640+AlexandreTorturela@users.noreply.github.com>
2025-12-28 20:52:00 +0000

    add `getSslValue()` at `database.js` to handle certificates

--- infra/database.js
+++ infra/database.js
@@ -7,8 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    //ssl: true,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSslValue(),
   });
 
   try {
@@ -26,3 +25,11 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSslValue() {
+  if (process.env.POSTGRES_CA) {
+    return { ca: process.env.POSTGRES_CA };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
