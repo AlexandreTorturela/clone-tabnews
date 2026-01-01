1e681079c75182f3101a65eda8a642d18d575334
Alexandre Torturela <154904640+AlexandreTorturela@users.noreply.github.com>
2026-01-01 16:00:22 +0000

    Concluído até migration dry run via api/v1/migration, método GET (até dia 23, 3 das 4 aulas concluídas)

--- infra/database.js
+++ infra/database.js
@@ -31,5 +31,5 @@ function getSslValue() {
     return { ca: process.env.POSTGRES_CA };
   }
 
-  return process.env.NODE_ENV === "development" ? false : true;
+  return process.env.NODE_ENV === "production" ? true : false;
 }
