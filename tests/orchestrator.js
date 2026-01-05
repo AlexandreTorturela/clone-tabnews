8031d4fdc0bd1f9dc8e9f1358cb043900960721d
AlexandreTorturela <torturela@gmail.com>
2026-01-04 23:57:07 -0300

    orquestração (dia 29)

---
+++ tests/orchestrator.js
@@ -0,0 +1,27 @@
+import retry from 'async-retry';
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch('http://localhost:3000/api/v1/status');
+      if (response.status != 200) {
+        throw Error();
+      }
+      //await response.json();
+      //const responseBody = response.json();
+      //console.log('endpoint api/v1/status: response.status=' + response.status);
+      //console.log('endpoint api/v1/status: response.json=' + responseBody.response);
+    }
+  }
+}
+
+export default {
+  waitForAllServices,
+};
