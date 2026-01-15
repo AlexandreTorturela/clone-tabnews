8843b6edd9402ec5d6168c77aa42121908871a31
AlexandreTorturela <torturela@gmail.com>
2026-01-15 19:18:17 -0300

    fix: fix migration file

--- infra/migrations/1768414550788_create-users.js
+++ infra/migrations/1768414550788_create-users.js
@@ -16,16 +16,18 @@ exports.up = (pgm) => {
       unique: true,
     },
     password: {
-      type: 'varchar(72)', //Why 72 in length? (bcrypt hashing truncates password size to 72)
+      type: 'varchar(60)', //Why 60 in length? (bcrypt hashing always returns hashes of size 60)
       notNull: true,
     },
     created_at: {
       type: 'timestamptz',
-      default: pgm.func('now()'),
+      default: pgm.func("timezone('utc',now())"),
+      notNull: true,
     },
     updated_at: {
       type: 'timestamptz',
-      default: pgm.func('now()'),
+      default: pgm.func("timezone('utc',now())"),
+      notNull: true,
     },
   });
 };
