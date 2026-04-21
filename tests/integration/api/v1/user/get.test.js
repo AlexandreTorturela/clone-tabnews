eb7049397896b9c3bfb2c93b015bffb1c1606ae1
AlexandreTorturela <torturela@gmail.com>
2026-04-21 15:47:09 -0300

    feat: add `sameSite=Lax` to `controller.setSessionCookie()`

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -76,6 +76,7 @@ describe('GET /api/v1/user', () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: '/',
         httpOnly: true,
+        sameSite: 'Lax',
       });
     });
 
@@ -127,6 +128,7 @@ describe('GET /api/v1/user', () => {
         maxAge: session.EXPIRATION_IN_MILLISECONDS / 1000,
         path: '/',
         httpOnly: true,
+        sameSite: 'Lax',
       });
     });
 
